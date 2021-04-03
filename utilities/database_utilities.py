feb8fb3f0970cf9326e5aca9e70880385deb10ac
mahaboobpathan <mahaboobpathan@outlook.com>
2021-04-03 01:02:06 -0400

    formatting issues

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,7 +1,8 @@
 def create_database(database_path: str):
-    #TODO: Generate the database
+    # TODO: Generate the database
     pass
 
+
 def save_words_to_database(database_path: str, words_list):
-    #TODO: save the words to the datebase
+    # TODO: save the words to the database
     pass
