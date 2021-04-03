0b8857a54862bd8ec78c8461b1fbf7a1e91a0b42
mahaboobpathan <mahaboobpathan@outlook.com>
2021-04-03 00:45:35 -0400

    Initial commit

---
+++ utilities/database_utilities.py
@@ -0,0 +1,7 @@
+def create_database(database_path: str):
+    #TODO: Generate the database
+    pass
+
+def save_words_to_database(database_path: str, words_list):
+    #TODO: save the words to the datebase
+    pass
